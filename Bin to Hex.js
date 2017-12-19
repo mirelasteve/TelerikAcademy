bd9f70c3feccd869c8885d9fa6692820babbe12f
mirelasteve <mirelasteve@abv.bg>
2017-12-19 08:40:04 +0200

    Update Bin to Hex.js

--- Bin to Hex.js
+++ Bin to Hex.js
@@ -2,7 +2,7 @@ const bin = gets();
 const binConvert = [];
 let start = bin.length - 4;
 let end = bin.length;
-for (let i = 0; i < bin.length; i += 4) {
+for (let i = 0; i <= bin.length; i += 4) {
     let a = bin.slice(start - i, end - i);
     if (a) {
         binConvert.push(a);
@@ -11,43 +11,43 @@ for (let i = 0; i < bin.length; i += 4) {
 const convertBinLen = 4 * binConvert.length;
 let last = bin.length - convertBinLen;
 if (last > 0) {
-    binConvert.push('0'.repeat(last) + bin.slice(0, last));
+    binConvert.push('0'.repeat(4 - last) + bin.slice(0, last));
 }
-let result = '';
+// print(last);
+let p = '';
 let workBin = binConvert.reverse();
 for (let i = 0; i < workBin.length; i += 1) {
     let binE = workBin[i];
-    let p = '';
     switch (binE) {
         case '0000':
-            p += 0;
+            p += '0';
             break;
         case '0001':
-            p += 1;
+            p += '1';
             break;
         case '0010':
-            p += 2;
+            p += '2';
             break;
         case '0011':
-            p += 3;
+            p += '3';
             break;
         case '0100':
-            p += 4;
+            p += '4';
             break;
         case '0101':
-            p += 5;
+            p += '5';
             break;
         case '0110':
-            p += 6;
+            p += '6';
             break;
         case '0111':
-            p += 7;
+            p += '7';
             break;
         case '1000':
-            p += 8;
+            p += '8';
             break;
         case '1001':
-            p += 9;
+            p += '9';
             break;
         case '1010':
             p += 'A';
@@ -70,9 +70,6 @@ for (let i = 0; i < workBin.length; i += 1) {
         default:
             break;
     }
-    result += p.toString();
 }
-// if (result[0] === 'u') {
-//     result = result.slice(8, );
-// }
-print(result);
+print(p);
+quit(0);
